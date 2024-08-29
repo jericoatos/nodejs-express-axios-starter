eff40375bae771edebb9bb663922b8eb1720baa2
jericoatos <jericoatos@gmail.com>
2024-08-29 11:52:22 +0100

    Add login system

--- test/unit/controllers/AuthControllerTest.ts
+++ test/unit/controllers/AuthControllerTest.ts
@@ -3,6 +3,7 @@ import * as AuthController from '../../../src/controllers/AuthController'
 import * as AuthService from '../../../src/services/AuthService';
 import { expect } from 'chai';
 import sinon from 'sinon';
+import { Request, Response } from 'express';
 
 describe('AuthController', function () {
 
@@ -15,95 +16,126 @@ describe('AuthController', function () {
 
     describe('logout', function () {
         it('should set token to undefined and redirect to login page', async () => {
-            const req = { session: {token: '' } };
-            const res = { redirect: sinon.spy(), locals: { } };
+            const req = {
+                session: { token: '' }
+            } as Partial<Request> as Request;
 
-            await AuthController.logout(req as any, res as any);
+            const res = {
+                redirect: sinon.spy()
+            } as Partial<Response> as Response;
 
-            expect(res.redirect.calledWith('/loginForm')).to.be.true;
+            await AuthController.logout(req, res);
+
+            expect((res.redirect as sinon.SinonSpy).calledWith('/loginForm')).to.be.true;
             expect(req.session.token).to.be.undefined;
-        })
-    })
+        });
+    });
 
     describe('getLoginForm', function () {
-
         it('should render loginForm', async () => {
-            const req = {};
-            const res = { render: sinon.spy(), locals: { loggedin: false } };
+            const req = {} as Request;
+            const res = {
+                render: sinon.spy(),
+                locals: { loggedin: false }
+            } as unknown as Response;
 
-            await AuthController.getLoginForm(req as any, res as any);
+            await AuthController.getLoginForm(req, res);
 
-            expect(res.render.calledOnce).to.be.true;
-            expect(res.render.calledWith('loginForm')).to.be.true;
-        })
-    })
-
+            expect((res.render as sinon.SinonSpy).calledOnce).to.be.true;
+            expect((res.render as sinon.SinonSpy).calledWith('loginForm')).to.be.true;
+        });
+    });
+    
+    
     describe('postLoginForm', function () {
-
-        // Succeeds on POST loginForm.
         it('should redirect when successfully retrieves JWT Token', async () => {
             sinon.stub(AuthService, 'getAuthToken').resolves("token");
-
-            // Need to ensure request body is populated with required content.
+    
             const req = {
                 body: {
-                    "email": VALID_EMAIL,
-                    "password": VALID_PASSWORD
-                }, session: {
-                    "token": ""
+                    email: VALID_EMAIL,
+                    password: VALID_PASSWORD
+                },
+                session: {
+                    token: ""
                 }
-            };
-
-            const res = { redirect: sinon.spy(), render: sinon.spy(), locals: { errormessage: '' }};
-
-            await AuthController.postLoginForm(req as any, res as any);
-
-            expect("token").to.equal(req.session.token);
-            expect(res.redirect.calledWith('/')).to.be.true;
-        })
-
-        // Fails if email is invalid
+            } as Partial<Request> as Request;
+    
+            const res = {
+                redirect: sinon.spy(),
+                render: sinon.spy(),
+                locals: { errormessage: '' }
+            } as Partial<Response> as Response;
+    
+            await AuthController.postLoginForm(req, res);
+    
+            expect(req.session.token).to.equal("token");
+            expect((res.redirect as sinon.SinonSpy).calledWith('/')).to.be.true;
+        });
+    
         it('should redirect to loginForm with errormessage and form data in body when email is invalid', async () => {
-            const error: string = "Error";
+            const error = "Error";
             sinon.stub(AuthService, 'getAuthToken').rejects(new Error(error));
-
+    
             const req = {
                 body: {
-                    "email": 'invalid',
-                    "password": VALID_PASSWORD
+                    email: 'invalid',
+                    password: VALID_PASSWORD
                 }
-            };
-
-            const res = { render: sinon.spy(), locals: { errormessage: '' } };
-
-            await AuthController.postLoginForm(req as any, res as any);
-
-            expect(res.render.calledWith('loginForm')).to.be.true;
+            } as Partial<Request> as Request;
+    
+            const res = {
+                render: sinon.spy(),
+                locals: { errormessage: '' }
+            } as Partial<Response> as Response;
+    
+            await AuthController.postLoginForm(req, res);
+    
+            expect((res.render as sinon.SinonSpy).calledWith('loginForm')).to.be.true;
             expect(res.locals.errormessage).to.equal(error);
             expect(req.body.email).to.equal('invalid');
             expect(req.body.password).to.equal(VALID_PASSWORD);
-        })
-
-        // Fails if password is invalid
+        });
+    
         it('should redirect to loginForm with errormessage and form data in body when password is invalid', async () => {
-            const error: string = "Error";
+            const error = "Error";
             sinon.stub(AuthService, 'getAuthToken').rejects(new Error(error));
-
+    
             const req = {
                 body: {
-                    "email": VALID_EMAIL,
-                    "password": 'invalid'
+                    email: VALID_EMAIL,
+                    password: 'invalid'
                 }
-            };
-
-            const res = { render: sinon.spy(), locals: { errormessage: '' } };
-
-            await AuthController.postLoginForm(req as any, res as any);
-
-            expect(res.render.calledWith('loginForm')).to.be.true;
+            } as Partial<Request> as Request;
+    
+            const res = {
+                render: sinon.spy(),
+                locals: { errormessage: '' }
+            } as Partial<Response> as Response;
+    
+            await AuthController.postLoginForm(req, res);
+    
+            expect((res.render as sinon.SinonSpy).calledWith('loginForm')).to.be.true;
             expect(res.locals.errormessage).to.equal(error);
             expect(req.body.password).to.equal('invalid');
             expect(req.body.email).to.equal(VALID_EMAIL);
-        })
-    })
-})
+        });
+    });
+    
+    describe('logout', function () {
+        it('should set token to undefined and redirect to login page', async () => {
+            const req = {
+                session: { token: '' }
+            } as Partial<Request> as Request;
+    
+            const res = {
+                redirect: sinon.spy()
+            } as Partial<Response> as Response;
+    
+            await AuthController.logout(req, res);
+    
+            expect((res.redirect as sinon.SinonSpy).calledWith('/loginForm')).to.be.true;
+            expect(req.session.token).to.be.undefined;
+        });
+    });
+})    
