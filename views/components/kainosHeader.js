fe3d92c2181947706f16e58eb0c9aa9f8d750fc2
John Hutchman <john_hutchman@hotmail.com>
2024-08-30 13:54:46 +0100

    making correction

--- views/components/kainosHeader.js
+++ views/components/kainosHeader.js
@@ -1,7 +1,7 @@
 /* global HTMLElement */
 /* global customElements */
 
-class MyHeader extends HTMLElement {
+class KainosHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <header>
@@ -31,31 +31,4 @@ class MyHeader extends HTMLElement {
     }
 }
 
-
-class MyFooter extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-           <footer class="text-light py-4">
-    <img src="../public/assets/images/kainoslogofooter.png" alt="Kainos logo" class="logo">
-    <div class="footer-content">
-        <div>
-            <p class="mb-3">Contact us:</p>
-            <div class="d-flex justify-content-center mb-4">
-                <a href="https://www.instagram.com/kainossoftware/" target="_blank" rel="noopener noreferrer" class="mx-2" aria-label="Instagram">
-                    <img src="../public/assets/images/instagramlogo.png" alt="Instagram Logo" width="50" height="50">
-                </a>
-                <a href="https://www.facebook.com/KainosSoftware/?locale=en_GB" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                    <img src="../public/assets/images/facebooklogo.png" alt="Facebook Logo" width="50" height="50">
-                </a>
-            </div>
-        </div>
-    </div>
-    <p class="mb-0">&copy; Kainos 2024</p>
-</footer>`;
-    }
-}
-
-
-customElements.define('special-header', MyHeader);
-
-customElements.define('special-footer', MyFooter);
+customElements.define('kainos-header', KainosHeader);
