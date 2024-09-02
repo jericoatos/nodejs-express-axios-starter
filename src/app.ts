9086f00335dba3f4730caae345b6a5159f441c54
Megan <meganc04@hotmail.co.uk>
2024-09-02 13:51:27 +0100

    branding fix 2.0

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ import session from "express-session";
 
 import { getAllDatabases } from "./controllers/TestController";
 
+//const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -29,4 +34,8 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getAllDatabases);
+app.get('/', async (req: express.Request, res: express.Response) => {
+  res.render("home.html");
+});
+
+app.get('/test', (() => getAllDatabases));
