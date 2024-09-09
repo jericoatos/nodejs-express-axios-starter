0ec87425f0e9c323aaaea6979b86b6ed43bfdffd
cormaco7 <cormac.okane@kainos.com>
2024-09-09 10:50:06 +0100

    added AWS test to JobRoleInfo

--- test/ui/UITests/jobRoleInfoTest.ts
+++ test/ui/UITests/jobRoleInfoTest.ts
@@ -16,6 +16,9 @@ describe('Job Role Information', function () {
 			        const element = await driver.findElement(By.xpath("//table[@id='jobRoleListTable']/tbody/tr/td/a"));
 			        const href = await element.getAttribute('href');
 
+                    await driver.findElement(By.xpath("//table[@id='jobRoleListTable']/tbody/tr/td/a")).click();
+                    await driver.sleep(1000);
+
                     const regex = /\d$/;
                     expect(href).to.match(regex, 'URL does not end with the ID number');
 
@@ -24,4 +27,28 @@ describe('Job Role Information', function () {
                 }
             });
     });
+    describe('AWS Job Role Information', function () {
+                it('Should display relevant Job Role Information', async function loginTest() {
+                    this.timeout(15000);
+                    const driver = await new Builder().forBrowser("chrome").build();
+                    try {
+                        await driver.get('https://jd5pri3k2r.eu-west-1.awsapprunner.com/');
+                        await driver.sleep(1000);
+                        await driver.findElement(By.id('careerslink')).click();
+                        await driver.sleep(1000);
+
+                        const element = await driver.findElement(By.xpath("//table[@id='jobRoleListTable']/tbody/tr/td/a"));
+                        const href = await element.getAttribute('href');
+
+                        await driver.findElement(By.xpath("//table[@id='jobRoleListTable']/tbody/tr/td/a")).click();
+                        await driver.sleep(1000);
+
+                        const regex = /\d$/;
+                        expect(href).to.match(regex, 'URL does not end with the ID number');
+
+                    } finally {
+                        await driver.quit();
+                    }
+                });
+        });
 })
