f814e263122ff328d6a76b170f5552637df8b8b9
cormaco7 <cormac.okane@kainos.com>
2024-09-10 11:01:30 +0100

    updated ui tests to reflect changes to main

--- test/ui/UITests/jobRoleInfoTest.ts
+++ test/ui/UITests/jobRoleInfoTest.ts
@@ -8,7 +8,11 @@ describe('Job Role Information', function () {
                 this.timeout(15000);
                 const driver = await new Builder().forBrowser("chrome").build();
                 try {
-                    await driver.get('http://localhost:3000');
+                    await driver.get('http://localhost:3000/loginForm');
+                    await driver.findElement(By.id('email')).sendKeys('user@email.com');
+                    await driver.findElement(By.id('password')).sendKeys('regularU$er123');
+                    await driver.sleep(500);
+                    await driver.findElement(By.id('submit')).click();
                     await driver.sleep(500);
                     await driver.findElement(By.id('careerslink')).click();
                     await driver.sleep(500);
@@ -32,10 +36,14 @@ describe('Job Role Information', function () {
                     this.timeout(15000);
                     const driver = await new Builder().forBrowser("chrome").build();
                     try {
-                        await driver.get('https://jd5pri3k2r.eu-west-1.awsapprunner.com/');
-                        await driver.sleep(500);
-                        await driver.findElement(By.id('careerslink')).click();
-                        await driver.sleep(500);
+                    await driver.get('https://jd5pri3k2r.eu-west-1.awsapprunner.com/loginForm');
+                    await driver.findElement(By.id('email')).sendKeys('user@email.com');
+                    await driver.findElement(By.id('password')).sendKeys('regularU$er123');
+                    await driver.sleep(500);
+                    await driver.findElement(By.id('submit')).click();
+                    await driver.sleep(500);
+                    await driver.findElement(By.id('careerslink')).click();
+                    await driver.sleep(500);
 
                         const element = await driver.findElement(By.xpath("//table[@id='jobRoleListTable']/tbody/tr/td/a"));
                         const href = await element.getAttribute('href');
