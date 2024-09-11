644195b9e1164569ac78622b949e1d1cd205b1fe
AshHarkinKainos <ashlea.harkin@kainos.com>
2024-09-11 22:47:04 +0100

    process and console undef lint errors fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,28 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist/", "node_modules/"]
+    ignores: ["dist/", "node_modules/"],
+    rules: {
+      // Disable the rule that disallows console statements
+      "no-console": "off",
+      // Ensure console is defined
+      "no-undef": "off",
+      // Disable the require-yield rule if not needed
+      "require-yield": "off",
+      // Ensure no var-requires rule is enforced
+      "@typescript-eslint/no-var-requires": "error",
+      // Additional TypeScript specific rules
+      "@typescript-eslint/explicit-module-boundary-types": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn"
+    },
+    languageOptions: {
+      globals: {
+        console: "readonly"
+      },
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    }
   }
 );
