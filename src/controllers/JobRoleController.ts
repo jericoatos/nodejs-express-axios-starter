64f102976f34762aca4f2a2148bea64d241c1831
jericoatos <jericoatos@gmail.com>
2024-09-11 15:43:36 +0100

    added role ordering

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -3,13 +3,16 @@ import { getJobRoleById, getJobRoles } from "../services/JobRoleService";
 
 
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
+    const orderBy = req.query.orderBy as string;
+    const direction = req.query.direction as string;
     try {
-        res.render('job-role-list', { jobRoles: await getJobRoles(req.session.token) });
-    } catch (e) {
-        res.locals.errorMessage = e.message;
-        res.render('job-role-list');
+        const jobRoles = await getJobRoles(req.session.token, orderBy, direction);
+        res.render('job-role-list', { jobRoles, orderBy, direction });
+    } catch (error) {
+        res.locals.errormessage = error.message;
+        res.render('job-role-list', { jobRoles: [], orderBy, direction });
     }
-}
+};
 
 export const getSingleJobRole = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
