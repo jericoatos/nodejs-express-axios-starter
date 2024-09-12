8edb489c5acbbc38e93b6556d97f0cef6defd099
AshHarkinKainos <ashlea.harkin@kainos.com>
2024-09-12 12:31:05 +0100

    added local urls

--- run-pa11y-tests.mjs
+++ run-pa11y-tests.mjs
@@ -4,7 +4,11 @@ import pa11y from 'pa11y';
 const urls = [
     "https://jd5pri3k2r.eu-west-1.awsapprunner.com/",
     "https://jd5pri3k2r.eu-west-1.awsapprunner.com/job-roles",
-    "https://jd5pri3k2r.eu-west-1.awsapprunner.com/loginForm"
+    "https://jd5pri3k2r.eu-west-1.awsapprunner.com/loginForm",
+    "http://localhost:3000/",
+    "http://localhost:3000/quiz",
+    "http://localhost:3000/loginErrorMessage",
+    "http://localhost:3000/loginForm"
 ];
 
 async function runPa11y() {
